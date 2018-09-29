6a89dd275c5e3709342d441ca32fb4151ff737dd
SnakeBigBoss <carlos.egmw@gmail.com>
2018-09-28 21:03:02 -0400

    commit inicial

---
+++ lib/tads6_carlos.rb
@@ -0,0 +1,5 @@
+require "tads6_carlos/version"
+
+module Tads6Carlos
+  # Your code goes here...
+end
---
+++ spec/tads6_carlos_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Tads6Carlos do
+  it "has a version number" do
+    expect(Tads6Carlos::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
